cbc9288e0436b3d7cbe7f4f1c172a64a7f15df7f
andyloweruk <andy.lower@applow.com>
2021-09-06 18:42:40 +0700

    All tests are passing locally.

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,10 @@
 module.exports = {
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
     local: {
       host: "localhost",
       port: 8545,
